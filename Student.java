d42d7e5f58e8ac6c0478a39f0d274cbeae1b3bdd
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-11 21:06:04 +0530

    Dhruv Sharma add:student

---
+++ Student.java
@@ -0,0 +1,36 @@
+class Student {
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+
+    public Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+
+    public static void main(String[] args) {
+        PostgraduateStudent pgStudent = new PostgraduateStudent(1931,"Dhruv Sharma",8.92);
+        pgStudent.display();
+    }
+}
+
+class PostgraduateStudent extends Student {
+    public PostgraduateStudent(int rollNumber, String name, double CGPA) {
+        super(rollNumber, name, CGPA);
+    }
+
+    public void display() {
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Name: " + name);
+        System.out.println("CGPA: " + getCGPA());
+    }
+}
